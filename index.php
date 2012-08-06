909eeaf0f14738e2cea064c08142b9ede5a87180
Carsten Keßler <carsten.kessler@uni-muenster.de>
2012-08-06 09:52:55 +0200

    Removed sparqlQueries.php
    Each query should go directly into the PHP script that uses it.

--- index.php
+++ index.php
@@ -75,5 +75,44 @@ include('sparqlQueries.php');
 	$customJS = emergencyQuery();
 	getFoot(array('jquery-ui-1.8.21.custom.min.js'), $customJS ); 
 
-
+	
+	/*
+	 * Send a sparql query to retreive the emergencies names and hide the result
+	 * in a span as a * splitable string.
+	 */
+	function emergencyQuery()
+	{
+	    $emergencies = sparqlQuery('SELECT DISTINCT ?uri ?label WHERE {
+	        GRAPH <http://hxl.humanitarianresponse.info/data/reference/fts-emergencies-2012> {
+	            ?uri hxl:commonTitle ?label .
+	        }
+	    }');
+	    
+	    $label = "label";
+	    $uri   = "uri";
+	
+		// we'll return the whole JS code here - if we only return the array of emergencies, PHP renders the array as a table instead of simply passing on the string :/
+		$elist = '
+		
+		/*
+		 * Provides the autocomplete function with an array of emergency names itself
+		 * provided by the emergency query php function.
+		 */
+		
+		$("#tags").autocomplete({
+		    source:[ ';
+		     
+	    foreach($emergencies as $emergency){
+	        $elist .= ' { label: "'.$emergency->$label.'", value: "'.$emergency->$uri.'"}, ' ;                 
+	    }
+	    
+	    // remove trailing comma:
+		//	$elist = substr($emergencies, 0, -2);    
+	             
+	    $elist .= ' {} ]
+	    	});
+	    ';
+	    
+	    return $elist;
+	}
 ?>
