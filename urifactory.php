b3980ec0176df7bd641ca115676bc8423a437841
Carsten Keßler <carsten.kessler@uni-muenster.de>
2012-08-22 09:07:25 +0200

    Script "urifactory.php" deleted
    We handle URI generation client-side.

--- urifactory.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// this script can be used via AJAX to generate URIs using the makeURI function in functions.php
-
-include_once('functions.php');
-
-echo makeURI($_POST["type"], $_POST["properties"]);
-
-?>
