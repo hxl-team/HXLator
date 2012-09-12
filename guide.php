4079d7051ea830ea6fa15a75c687a0f0a728499c
Carsten Keßler <carsten.kessler@uni-muenster.de>
2012-09-12 14:46:04 +0200

    First version of cheat sheet added.

--- guide.php
+++ guide.php
@@ -16,5 +16,6 @@ getHead("guide.php");
 	<a href="https://sites.google.com/site/hxlproject/"><img src="img/hxl-logo-s.png" align="right" /></a>
 	<h1>Quick Start Guide <i class="icon-info-sign"></i></h1>
 	<p style="margin-top:  1.5em; margin-bottom:  1.5em">Select <a href="index.php">HXLate</a> to get started!</p>	 
+	<p>There is also a <a href="cheatsheet.html" target="_blank">cheat sheet</a> available for all technical terms used in HXLator.</p>
 
 <?php getFoot(); ?>
